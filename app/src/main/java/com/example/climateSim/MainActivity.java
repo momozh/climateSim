ecd520977bf27062447326fb580f49104440c2e7
YumoZ <momozhang.qiur@gmail.com>
2016-11-21 17:31:41 +0800

    temp commit 531pm

--- app/src/main/java/com/example/climateSim/MainActivity.java
+++ app/src/main/java/com/example/climateSim/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
         main_msc.setLooping(true);
         main_msc.start();
 
-
         Intent splash = new Intent(this,Splash.class);
 
         startActivityForResult(splash,4);
@@ -104,6 +103,13 @@ public class MainActivity extends AppCompatActivity {
     //@Override
     //protected void onResume() {
     //    super.onResume();
+    //    View decorView = getWindow().getDecorView();
+    //    // Hide both the navigation bar and the status bar.
+    //    // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+    //    // a general rule, you should design your app to hide the status bar whenever you
+    //    // hide the navigation bar.
+    //    int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+    //    decorView.setSystemUiVisibility(uiOptions);
     //}
 
     public void onNextTurn(View v)
